1b5e85fc2f8e6595e5b6c7efcc9c02595bc08794
Wim De Meester <wim.demeester@ster.kuleuven.be>
2016-09-17 22:38:02 +0200

    Show link to Startup Wizard in navigation drawer.

--- app/src/main/java/org/deepskylog/vela/MainActivity.java
+++ app/src/main/java/org/deepskylog/vela/MainActivity.java
@@ -71,15 +71,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } 
+        if (id == R.id.wizard) {
+            // Handle the startup wizard action
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
