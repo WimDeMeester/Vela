e4d2df80b097659fd222eb21334ae93bc5708dd6
Wim De Meester <wim.demeester@ster.kuleuven.be>
2016-10-06 21:08:08 +0200

    Check if the device has a bluetooth adapter

--- app/src/main/java/org/deepskylog/vela/util/Android.java
+++ app/src/main/java/org/deepskylog/vela/util/Android.java
@@ -1,8 +1,40 @@
 package org.deepskylog.vela.util;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 /**
  * Created by wim on 06/10/2016.
  */
 
 public class Android {
+    /**
+     * Show a dialog and sets the title, message and button name. Nothing happens when the button is
+     * clicked
+     * @param act The activity where the dialog should be shown
+     * @param message The message to show
+     * @param title The title to show
+     * @param button The text of the button
+     */
+    public static void showDialog(Activity act, int message, int title, int button) {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(act);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(message)
+                .setTitle(title);
+
+        builder.setPositiveButton(button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Nothing happens
+            }
+        });
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
+    }
 }
--- app/src/main/java/org/deepskylog/vela/wizard/Bluetooth.java
+++ app/src/main/java/org/deepskylog/vela/wizard/Bluetooth.java
@@ -1,16 +1,21 @@
 package org.deepskylog.vela.wizard;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
 import org.deepskylog.vela.R;
+import org.deepskylog.vela.util.Android;
 
 public class Bluetooth extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO: Check if bluetooth is enabled. If this is the case, change the button and make the button unselectable
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wizard_bluetooth);
     }
@@ -21,6 +26,18 @@ public class Bluetooth extends AppCompatActivity {
     }
 
     public void enableBluetoothOnClick(View v) {
+        // Check if the device has a bluetooth adapter
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Android.showDialog(this, R.string.noBluetooth, R.string.noBluetoothTitle, R.string.ok);
+        }
+
+        // TODO: Turn on bluetooth
+
+        // TODO: Change the button to show that bluetooth is enabled.
+        // TODO: Make the button unselectable
+
         //Intent intent = new Intent(this, Bluetooth.class);
         //startActivity(intent);
     }
